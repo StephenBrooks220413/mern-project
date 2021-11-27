dc6a6feeb384fb11f7beee504c98585ff30c083c
StephenBrooks220413 <krammer439@gmail.com>
2021-11-26 18:11:49 -0800

    updated file changes for mern project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [user, setUser] = useState(" ");
+    const fetchUserData = () => {
+        fetch(`http://localhost:8000/api`, {
+            method: "GET",
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+    };
+    return (
+        <>
+            <div className={"container"}>
+                <button onClick={fetchUserData}
+                        className={"btn btn-primary"}>
+                    submit
+                </button>
+            </div>
+        </>
+    )
 }
 
-export default App;
+
+// import {useState} from 'react';
+// import './App.css';
+// import { MDBContainer} from 'mdb-react-ui-kit';
+//
+// function App() {
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+//         console.log('STATE => ', email , password);
+//     };
+//
+//   return (
+//     <MDBContainer className={"mt-5 text-center"}>
+//         <br/><br/><br/>
+//      <h1 className={"display-4"}>Learning React</h1>
+//         <br/>
+//         <div className={"row"}>
+//             <div className={"col-md-12"}>
+//                 <form onSubmit={handleSubmit}>
+//                     <div className="form-outline mb-4">
+//                         <input value={email}
+//                                onChange={(event) => setEmail(event.target.value)}
+//                                type="email"
+//                                className="form-control"/>
+//
+//                     </div>
+//
+//                     <div className="form-outline mb-4">
+//                         <input value={password}
+//                                type="password"
+//                                onChange={(event) => setPassword(event.target.value)}
+//                                className="form-control"/>
+//
+//                     </div>
+//
+//                     <button type="submit" className="btn btn-primary btn-block">Sign in</button>
+//                 </form>
+//             </div>
+//         </div>
+//     </MDBContainer>
+//   );
+// }
+//
+// export default App;
